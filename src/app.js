a64b687bdd560b269bac4ab6761b91ada68d68ad
Caiorossi00 <caio_rossi56@hotmail.com>
2025-06-13 17:44:59 -0300

    feat: enhance book retrieval and management endpoints with improved error handling

--- src/app.js
+++ src/app.js
@@ -31,50 +31,25 @@ app.get("/books", async (req, res) => {
   }
 });
 
-app.post("/books", async (req, res) => {
-  try {
-    const newBook = req.body;
-    const result = await booksCollection.insertOne(newBook);
-    res.status(201).json({ ...newBook, _id: result.insertedId });
-  } catch (error) {
-    res.status(500).json({ error: "Erro ao adicionar o livro" });
-  }
-});
-
-app.put("/books/:id", async (req, res) => {
+app.get("/books/:id", async (req, res) => {
   const { id } = req.params;
-  const updatedBook = req.body;
-  try {
-    const result = await booksCollection.updateOne(
-      { _id: new ObjectId(id) },
-      { $set: updatedBook }
-    );
 
-    if (result.matchedCount === 0) {
-      return res.status(404).json({ error: "Livro não encontrado" });
-    }
-
-    res.status(200).json({ message: "Livro atualizado com sucesso" });
-  } catch (error) {
-    res.status(500).json({ error: "Erro ao atualizar o livro" });
+  if (!ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "ID inválido" });
   }
-});
 
-app.delete("/books/:id", async (req, res) => {
-  const { id } = req.params;
   try {
-    const result = await booksCollection.deleteOne({
+    const book = await booksCollection.findOne({
       _id: new ObjectId(id),
     });
 
-    if (result.deletedCount === 0) {
+    if (!book) {
       return res.status(404).json({ error: "Livro não encontrado" });
     }
 
-    res.status(200).json({ message: "Livro deletado com sucesso" });
+    res.json(book);
   } catch (error) {
-    console.error("Erro ao deletar:", error);
-    res.status(500).json({ error: "Erro ao deletar o livro" });
+    res.status(500).json({ error: "Erro ao buscar o livro" });
   }
 });
 
@@ -86,24 +61,6 @@ app.get("/books/destaques", async (req, res) => {
     res.json(destaques);
   } catch (error) {
     res.status(500).json({ error: "Erro ao buscar os destaques" });
-  }
-});
-
-app.get("/books/:id", async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const book = await booksCollection.findOne({
-      id: Number(id),
-    });
-
-    if (!book) {
-      return res.status(404).json({ error: "Livro não encontrado" });
-    }
-
-    res.json(book);
-  } catch (error) {
-    res.status(500).json({ error: "Erro ao buscar o livro" });
   }
 });
 
@@ -122,5 +79,88 @@ app.get("/books/genero/:genero", async (req, res) => {
     res.json(books);
   } catch (error) {
     res.status(500).json({ error: "Erro ao buscar os livros por gênero" });
+  }
+});
+
+app.post("/books", async (req, res) => {
+  try {
+    const newBook = req.body;
+    const result = await booksCollection.insertOne(newBook);
+    res.status(201).json({ ...newBook, _id: result.insertedId });
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao adicionar o livro" });
+  }
+});
+
+app.put("/books/:id", async (req, res) => {
+  const { id } = req.params;
+  const updatedBook = req.body;
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "ID inválido" });
+  }
+
+  try {
+    const result = await booksCollection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: updatedBook }
+    );
+
+    if (result.matchedCount === 0) {
+      return res.status(404).json({ error: "Livro não encontrado" });
+    }
+
+    res.status(200).json({ message: "Livro atualizado com sucesso" });
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao atualizar o livro" });
+  }
+});
+
+app.patch("/books/:id", async (req, res) => {
+  const { id } = req.params;
+  const updatedFields = req.body;
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "ID inválido" });
+  }
+
+  try {
+    const result = await booksCollection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: updatedFields }
+    );
+
+    if (result.matchedCount === 0) {
+      return res.status(404).json({ error: "Livro não encontrado" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "Livro atualizado parcialmente com sucesso" });
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao atualizar parcialmente o livro" });
+  }
+});
+
+app.delete("/books/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "ID inválido" });
+  }
+
+  try {
+    const result = await booksCollection.deleteOne({
+      _id: new ObjectId(id),
+    });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ error: "Livro não encontrado" });
+    }
+
+    res.status(200).json({ message: "Livro deletado com sucesso" });
+  } catch (error) {
+    console.error("Erro ao deletar:", error);
+    res.status(500).json({ error: "Erro ao deletar o livro" });
   }
 });
